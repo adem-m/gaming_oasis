725118e683aee956ac5888fa868e5b38f7b19606
Adem Mrizak <adem.mrizak@gmail.com>
2023-04-14 12:34:33 +0200

    Test rename

--- src/test/java/org/esgi/domain/NoteTest.java
+++ src/test/java/org/esgi/domain/NoteTest.java
@@ -7,14 +7,14 @@ import static org.junit.Assert.assertEquals;
 public class NoteTest {
 
     @Test
-    public void testNote() {
+    public void uneNoteDe10DoitAvoirLaValeur10() {
         Note note = Note.de(10);
 
         assertEquals(10, note.getValeur(), 0.0);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testNoteNegative() {
+    public void uneNoteNegativeDoitLeverUneException() {
         Note.de(-1);
     }
 }
